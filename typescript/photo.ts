09d45ab6ac78441e3f12f8d8d5ac57f8c1dd60cd
Chen Buyi <chenbuyi2019@outlook.com>
2022-12-03 14:57:39 +0800

    u

--- typescript/photo.ts
+++ typescript/photo.ts
@@ -1,8 +1,15 @@
 /// <reference path="utils.ts" />
 
 (function () {
+    const maxPhotoIndex = 12
+    const minPhotoIndex = 0
+    const photoIndexArray: number[] = []
+    for (let index = minPhotoIndex; index <= maxPhotoIndex; index++) {
+        photoIndexArray.push(index)
+    }
+    DisorderArray(photoIndexArray)
+
     const divPhoto = document.getElementById('photo') as HTMLDivElement
-    let lastPhotoIndex = GetRandInt(1, 12)
     const imgPhoto = document.createElement('img')
     divPhoto.appendChild(imgPhoto)
 
@@ -11,14 +18,12 @@
         if (photoChangedTime > 1) { window.scrollBy(0, window.innerHeight) }
     })
 
+    let lastPhotoIndex = 0
     function changePhoto() {
         photoChangedTime += 1
-        let newIndex = lastPhotoIndex
-        while (newIndex == lastPhotoIndex) {
-            newIndex = GetRandInt(1, 12)
-        }
-        lastPhotoIndex = newIndex
-        imgPhoto.src = `/photos/a${lastPhotoIndex.toFixed().padStart(2, '0')}.webp`
+        lastPhotoIndex += 1
+        if (lastPhotoIndex > maxPhotoIndex) { lastPhotoIndex = minPhotoIndex }
+        imgPhoto.src = `/photos/a${photoIndexArray[lastPhotoIndex].toFixed().padStart(2, '0')}.webp`
     }
     changePhoto()
     imgPhoto.addEventListener('click', changePhoto)
--- www/main.js
+++ www/main.js
@@ -68,8 +68,14 @@ function DisorderArray(array) {
     ]);
 })();
 (function () {
+    const maxPhotoIndex = 12;
+    const minPhotoIndex = 0;
+    const photoIndexArray = [];
+    for (let index = minPhotoIndex; index <= maxPhotoIndex; index++) {
+        photoIndexArray.push(index);
+    }
+    DisorderArray(photoIndexArray);
     const divPhoto = document.getElementById('photo');
-    let lastPhotoIndex = GetRandInt(1, 12);
     const imgPhoto = document.createElement('img');
     divPhoto.appendChild(imgPhoto);
     let photoChangedTime = 0;
@@ -78,14 +84,14 @@ function DisorderArray(array) {
             window.scrollBy(0, window.innerHeight);
         }
     });
+    let lastPhotoIndex = 0;
     function changePhoto() {
         photoChangedTime += 1;
-        let newIndex = lastPhotoIndex;
-        while (newIndex == lastPhotoIndex) {
-            newIndex = GetRandInt(1, 12);
+        lastPhotoIndex += 1;
+        if (lastPhotoIndex > maxPhotoIndex) {
+            lastPhotoIndex = minPhotoIndex;
         }
-        lastPhotoIndex = newIndex;
-        imgPhoto.src = `/photos/a${lastPhotoIndex.toFixed().padStart(2, '0')}.webp`;
+        imgPhoto.src = `/photos/a${photoIndexArray[lastPhotoIndex].toFixed().padStart(2, '0')}.webp`;
     }
     changePhoto();
     imgPhoto.addEventListener('click', changePhoto);
